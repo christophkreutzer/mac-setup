00c98cdae00b6c9e6181102f7d2d27ca739e5732
Christoph Kreutzer <christophkreutzer@users.noreply.github.com>
2020-03-08 20:35:55 +0100

    Add Ansible setup playbook

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+
+if [[ ! -x /usr/local/bin/brew ]]; then
+  echo "Setup Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  brew cask
+fi
+
+echo "Setup Ansible..."
+brew install ansible
+
+echo "All done. You may now run the setup playbook!"
+echo "  ansible-playbook -i hosts setup.yml"
+
