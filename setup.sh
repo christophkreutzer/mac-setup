7f5be904506bc35597ddb67ad33cdcdab8798f93
Christoph Kreutzer <christophkreutzer@users.noreply.github.com>
2021-04-13 21:12:31 +0200

    Fix homebrew cask (no longer necessary)

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ set -eu
 if [[ ! -x /usr/local/bin/brew ]]; then
   echo "Setup Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  brew cask
 fi
 
 echo "Setup Ansible..."
